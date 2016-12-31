f7c6f362543666d2f0adb07ad51003df1cc28242
jmarsalis <jmarsalis@gmail.com>
2016-12-31 09:26:39 -0600

    added travis ci yml file and build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# exit if any command fails
+set -e
+
+# the location of the output of the build output (must be an absolute path)
+artifactsFolder="~/src/artifacts/dotnet-core-sandbox"
+
+# remove the drop location if it exists
+if [ -d $artifactsFolder ]; then  
+  rm -R $artifactsFolder
+fi
+
+# get the revision number for the version from the travis job
+revision=${TRAVIS_JOB_ID:=1}  
+revision=$(printf "%04d" $revision)
+
+# build the application
+dotnet publish ./project.json -c Release -o $artifactsFolder --version-suffix=$revision
